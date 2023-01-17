8b5198ccad2eb38c8639b886fc5154f4529e6ed7
Abdurrahman Muhammad Kabir <fadlumuhd570@gmail.com>
2023-01-17 11:55:08 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,3 +4,7 @@
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+
+//you can use _FILE_ instead of typing the actual filename. _FILE_ is a pre
+//defined macro that detects the filename of the current working file
+//it gets replaced with the actual filename 
